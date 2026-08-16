df1e086c52074be0f57004e4e1c6e3bdf1b05d20
trivsaviral150605@gmail.com <trivsaviral150605@gmail.com>
2026-08-17 00:22:09 +0530

    Fix User model filename

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,6 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
